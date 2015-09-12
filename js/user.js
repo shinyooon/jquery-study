84b21bcfb684a07e0c992918fc44cf137ea0d377
shinyooon <talkyfull@gmail.com>
2015-09-12 11:20:13 +0900

    login함수 완성

--- js/user.js
+++ js/user.js
@@ -34,6 +34,9 @@ var user  = {
 		this.$el.find('#btnSubmit').click(function(){
 			user.signUp();
 		});
+		this.$el.find('#btnLogin').click(function(){
+			user.login();
+		});
 	},
 	showModal : function(){
 		user.resetModal();
@@ -66,11 +69,20 @@ var user  = {
 		}
 
 		// 3. 이미 등록된 사용자가 아닌가?
-		if(this.find(email)){
+		if(this.find(email)[0]){
 			alert('이메일이 중복됩니다.')
 			return;
 		}
 		// 4. 위 검증이 끝나면 회원 가입
+		var currentTime = new Date();
+		this.save({
+			email : email,
+			password : password,
+			name : name,
+			job : job,
+			joinDate : currentTime,
+			updateDate : currentTime
+		});
 	},
 
 	validate : function(){
@@ -92,11 +104,15 @@ var user  = {
 	},
 	find : function(email){
 		//email중복되는지 체크하고 같은 이메일이 있다면 true 없다면 false;
-		var result = false; //false값을 넣지 않으면 undefined이고 javascript 는 undifined를 false.
-		for(var i = 0;i<users.length;i++){
+		var i = 0;
+		var result = null; //false값을 넣지 않으면 undefined이고 javascript 는 undifined를 false.
+
+		for(i = 0;i<users.length;i++){
 			if(users[i].email===email){
-				result  = true;
+				result  = users[i];
 				break;
+			}else{
+				result = null;
 			}
 		}
 		/*
@@ -105,10 +121,38 @@ var user  = {
 		$.each(users,function(index, value){
 			if(users[index].email===email){
 				result  = true;
+				return;
 			}
 		})
 		*/
 		return result;
+	},
+	save : function(obj){
+		users.push(obj);
+		alert('등록되었습니다.');
+		this.closeModal();
+	},
+	login:function(){
+		//입력창에 입력된 email과 password를 검사해서 일치하면 로그인  아니면 email & password 확인 alert
+		var email = this.$el.find('#loginEmail').val(),
+			password = this.$el.find('#loginPassword').val();
+			result = true;
+		var user = this.find(email);
+		if(this.find(email)){
+			if(user.password === password){
+				result=true;
+			}else{
+				result=false;
+			}
+		}else{
+			result=false;
+		}
+
+		if(result){
+			alert('로그인')
+		}else{
+			alert('아이디와 비밀번호를 확인해주세요.');
+		}
 	}
 
 
